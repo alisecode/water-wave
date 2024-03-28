f849d6ca5934d21b2bc50084c52d486882c447ea
Alisa <alisa.melissa@icloud.com>
2024-03-28 19:06:09 +0200

    Initial Commit

---
+++ WaterBalance/WaterBalanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterBalanceApp.swift
+//  WaterBalance
+//
+//  Created by Алиса Третьякова on 28.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct WaterBalanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
